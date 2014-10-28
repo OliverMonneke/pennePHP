71373fe17a1fd70127205c8ab5db82892376aa7b
Oliver Monneke <oliver.monneke@uk-koeln.de>
2014-10-28 12:33:44 +0100

    type check

--- src/Datatype/String.php
+++ src/Datatype/String.php
@@ -62,8 +62,8 @@ class String implements DatatypeInterface
      * Get a part of a string
      *
      * @param string $string The string
-     * @param int    $start  Starting point
-     * @param int    $length Length
+     * @param int $start Starting point
+     * @param int $length Length
      *
      * @return string
      */
@@ -95,9 +95,9 @@ class String implements DatatypeInterface
     /**
      * Check if string starts with another string
      *
-     * @param string $needle        String to look for
-     * @param string $haystack      String to check
-     * @param bool   $caseSensitive Case sensitive check
+     * @param string $needle String to look for
+     * @param string $haystack String to check
+     * @param bool $caseSensitive Case sensitive check
      *
      * @return bool
      */
@@ -109,15 +109,19 @@ class String implements DatatypeInterface
 
         list($needle, $haystack) = self::transformCase($needle, $haystack, $caseSensitive);
 
-        return (self::substring($haystack, 0, self::length($needle)) === $needle);
+        if ($needle === false) {
+            return false;
+        } else {
+            return (self::substring($haystack, 0, self::length($needle)) === $needle);
+        }
     }
 
     /**
      * Check if string ends with another string
      *
-     * @param string $needle        String to look for
-     * @param string $haystack      String to check
-     * @param bool   $caseSensitive Case sensitive check
+     * @param string $needle String to look for
+     * @param string $haystack String to check
+     * @param bool $caseSensitive Case sensitive check
      *
      * @return bool
      */
@@ -131,9 +135,9 @@ class String implements DatatypeInterface
     /**
      * Check if string contains another string
      *
-     * @param string $needle        String to look for
-     * @param string $haystack      String to check
-     * @param bool   $caseSensitive Case sensitive check
+     * @param string $needle String to look for
+     * @param string $haystack String to check
+     * @param bool $caseSensitive Case sensitive check
      *
      * @return bool
      */
@@ -151,10 +155,10 @@ class String implements DatatypeInterface
     /**
      * Replace a string with another string
      *
-     * @param string $search        The string to look for
-     * @param string $replace       The replacement string
-     * @param string $string        The string to check
-     * @param bool   $caseSensitive Case sensitive replacement
+     * @param string $search The string to look for
+     * @param string $replace The replacement string
+     * @param string $string The string to check
+     * @param bool $caseSensitive Case sensitive replacement
      *
      * @return string|bool
      */
@@ -181,7 +185,8 @@ class String implements DatatypeInterface
     public static function isEmpty($string)
     {
         if (!self::isValid($string) &&
-            null !== $string) {
+            null !== $string
+        ) {
             return false;
         }
 
@@ -198,7 +203,8 @@ class String implements DatatypeInterface
     public static function isNotEmpty($string)
     {
         if (!self::isValid($string) &&
-            null !== $string) {
+            null !== $string
+        ) {
             return false;
         }
 
@@ -211,7 +217,7 @@ class String implements DatatypeInterface
      */
     public static function lowerFirst($string)
     {
-        return self::lower(self::substring($string, 0, 1)).self::substring($string, 1);
+        return self::lower(self::substring($string, 0, 1)) . self::substring($string, 1);
 
     }
 
@@ -221,7 +227,7 @@ class String implements DatatypeInterface
      */
     public static function upperFirst($string)
     {
-        return self::upper(self::substring($string, 0, 1)).self::substring($string, 1);
+        return self::upper(self::substring($string, 0, 1)) . self::substring($string, 1);
     }
 
     /**
