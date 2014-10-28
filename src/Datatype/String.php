80e3a37f8a71448161ed330aaa1db3f1720a396d
Oliver Monneke <oliver.monneke@uk-koeln.de>
2014-10-28 12:45:26 +0100

    remove unneeded code

--- src/Datatype/String.php
+++ src/Datatype/String.php
@@ -202,12 +202,6 @@ class String implements DatatypeInterface
      */
     public static function isNotEmpty($string)
     {
-        if (!self::isValid($string) &&
-            null !== $string
-        ) {
-            return false;
-        }
-
         return (!self::isEmpty($string));
     }
 
